8b36bb94468378ef29f995e738390651836d503a
Preston Peek <36671440+dprestonpeek@users.noreply.github.com>
2025-05-28 13:51:31 -0400

    Music based on 3 tracks now instead of 11

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -11,22 +11,6 @@ public class GameManager : MonoBehaviour
     AudioClip track2;
     [SerializeField]
     AudioClip track3;
-    [SerializeField]
-    AudioClip track4;
-    [SerializeField]
-    AudioClip track5;
-    [SerializeField]
-    AudioClip track6;
-    [SerializeField]
-    AudioClip track7;
-    [SerializeField]
-    AudioClip track8;
-    [SerializeField]
-    AudioClip track9;
-    [SerializeField]
-    AudioClip track10;
-    [SerializeField]
-    AudioClip track11;
 
     // Start is called before the first frame update
     void Start()
@@ -39,6 +23,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Is there a better way to do this? Especially with more tracks
         if (!audioSource.isPlaying)
         {
             if (audioSource.clip == track1)
@@ -50,38 +35,6 @@ public class GameManager : MonoBehaviour
                 audioSource.clip = track3;
             }
             else if (audioSource.clip == track3)
-            {
-                audioSource.clip = track4;
-            }
-            else if (audioSource.clip == track4)
-            {
-                audioSource.clip = track5;
-            }
-            else if (audioSource.clip == track5)
-            {
-                audioSource.clip = track6;
-            }
-            else if (audioSource.clip == track6)
-            {
-                audioSource.clip = track7;
-            }
-            else if (audioSource.clip == track7)
-            {
-                audioSource.clip = track8;
-            }
-            else if (audioSource.clip == track8)
-            {
-                audioSource.clip = track9;
-            }
-            else if (audioSource.clip == track9)
-            {
-                audioSource.clip = track10;
-            }
-            else if (audioSource.clip == track10)
-            {
-                audioSource.clip = track11;
-            }
-            else if (audioSource.clip == track11)
             {
                 audioSource.clip = track1;
             }
